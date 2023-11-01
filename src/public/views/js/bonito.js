310354657191e0ec2fdac929db27233466fe0ea2
Matheus <131892555+eroesy@users.noreply.github.com>
2023-11-01 18:22:49 -0300

    estilo fodas
    sdadsad

--- src/public/views/js/bonito.js
+++ src/public/views/js/bonito.js
@@ -1,28 +1,13 @@
-var bar = document.querySelector('.menu')
-var img1 = document.querySelector('#img1')
-var img2 = document.querySelector('#img2')
-var txt1 = document.querySelector('#txt1')
-var txt2 = document.querySelector('#txt2')
+const icons = document.querySelector(".icons");
+const texts = document.querySelector(".texts");
+const resto = document.querySelector(".resto");
 
+resto.addEventListener("mouseover", () => {
+    icons.style.display = "none";
+    texts.style.display = "flex";
+});
 
-bar.addEventListener("mouseover", function(){
-    setTimeout(function() {
-        img1.style.display = ' inline-block'
-        img2.style.display = ' inline-block'
-        txt1.style.display = 'none'
-        txt2.style.display = 'none'
-      }, 100);
-    
-    
-})
-
-bar.addEventListener("mouseout", function(){
-    setTimeout(function() {
-        img1.style.display = ' none'
-        img2.style.display = ' none'
-        txt1.style.display = 'inline-block'
-        txt2.style.display = 'inline-block'
-    
-      }, 100);
-    
-})
+resto.addEventListener("mouseout", () => {
+    icons.style.display = "flex";
+    texts.style.display = "none";
+});
